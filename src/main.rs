2279fcb85199a290e22060253e13af6d32619e91
EliaGorokhovsky <eliapgorokhovsky@gmail.com>
2023-06-23 05:34:36 -0600

    Add drawing based on Delaunay triangulations

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,20 @@ use std::f32::consts::TAU;
 use rand::{distributions::Distribution, Rng};
 
 // This is only used for .after()
-use bevy::{
-	prelude::*,
-	transform,
-};
+use bevy::{prelude::*, transform};
+
+use spade::Triangulation;
 
 const NUM_NODES: usize = 20;
 const NUM_FIXED_NODES: usize = 30;
 const BARYCENTER_METHOD_RADIUS: f32 = 275.0;
-const P_EDGE: f64 = 0.1;
+const P_EDGE: f64 = 0.75;
 const PHYSICS_ITERATIONS: u32 = 100;
-const SPRING_LENGTH: f32 = 0.2;
-const SPRING_FORCE: f32 = 100.0;
+const SPRING_LENGTH: f32 = 100.0;
+const SPRING_FORCE: f32 = 10000.0;
 const SPRING_SCALE: f32 = 0.01;
-const ELECTRIC_FORCE: f32 = 1000000.0;
-const WALL_REPULSION: f32 = 100000.0;
+const ELECTRIC_FORCE: f32 = 0.0;
+const WALL_REPULSION: f32 = 1000.0;
 const WINDOW_WIDTH: f32 = 500.0;
 const WINDOW_HEIGHT: f32 = 300.0;
 const PROXIMITY_FACTOR: f32 = 13.0;
@@ -41,7 +40,7 @@ struct Configuration {
 	spring_length: f32,
 	spring_force: f32,
 	spring_scale: f32,
-	electric_force: f32
+	electric_force: f32,
 }
 
 #[derive(Resource)]
@@ -62,10 +61,7 @@ fn setup(mut commands: bevy::prelude::Commands, server: Res<AssetServer>) {
 	});
 }
 
-fn make_nodes(
-	mut commands: bevy::prelude::Commands,
-	image_handles: Res<PlanetImages>
-) {
+fn make_nodes(mut commands: bevy::prelude::Commands, image_handles: Res<PlanetImages>) {
 	let mut rng = rand::thread_rng();
 	for _ in 0..NUM_NODES {
 		let x = rng.gen_range(-WINDOW_WIDTH..WINDOW_WIDTH);
@@ -131,13 +127,18 @@ fn make_edges(
 	}
 }
 
-fn make_edges_by_proximity(mut commands: bevy::prelude::Commands,
+fn make_edges_by_proximity(
+	mut commands: bevy::prelude::Commands,
 	mut query: bevy::prelude::Query<(
 		bevy::ecs::entity::Entity,
 		&mut Nodes,
 		&bevy::prelude::Transform,
-	)>,) {
-	let minimum = query.iter_combinations::<2>().map(|[(_, _, &t1), (_, _, &t2)]| t1.translation.distance(t2.translation)).fold(WINDOW_WIDTH, |x, running_min| x.min(running_min));
+	)>,
+) {
+	let minimum = query
+		.iter_combinations::<2>()
+		.map(|[(_, _, &t1), (_, _, &t2)]| t1.translation.distance(t2.translation))
+		.fold(WINDOW_WIDTH, |x, running_min| x.min(running_min));
 	let mut pairs = query.iter_combinations_mut::<2>();
 	while let Some([(e1, mut n1, &t1), (e2, mut n2, &t2)]) = pairs.fetch_next() {
 		let dist = t1.translation.distance(t2.translation);
@@ -170,14 +171,101 @@ fn make_edges_by_proximity(mut commands: bevy::prelude::Commands,
 	}
 }
 
-fn fix_nodes(mut query: bevy::prelude::Query<(
-	&Nodes,
-	&mut bevy::prelude::Transform,
-), Without<Edge>>) {
+struct EntityWithPosition {
+	entity: Entity,
+	position: spade::Point2<f32>,
+}
+
+impl spade::HasPosition for EntityWithPosition {
+	type Scalar = f32;
+
+	fn position(&self) -> spade::Point2<f32> {
+		self.position
+	}
+}
+
+fn make_graph_delaunay(mut commands: bevy::prelude::Commands, image_handles: Res<PlanetImages>) {
+	let distribution = rand::distributions::Bernoulli::new(P_EDGE).unwrap();
+	let mut rng = rand::thread_rng();
+	let mut triangulation: spade::DelaunayTriangulation<EntityWithPosition> =
+		spade::DelaunayTriangulation::new();
+	for _ in 0..NUM_NODES {
+		let x = rng.gen_range(-WINDOW_WIDTH..WINDOW_WIDTH);
+		let y = rng.gen_range(-WINDOW_HEIGHT..WINDOW_HEIGHT);
+		let pos: bevy::math::Vec3 = bevy::math::Vec3 { x, y, z: 0.0 }; // Remove 0.0 on x and y for random init positions
+		let entity = commands
+			.spawn((
+				Nodes { entries: vec![] },
+				bevy::sprite::SpriteBundle {
+					texture: image_handles.default.clone(),
+					transform: bevy::prelude::Transform::from_translation(pos),
+					..bevy::utils::default()
+				},
+				Movement {
+					velocity: Vec3 {
+						x: 0.0,
+						y: 0.0,
+						z: 0.0,
+					},
+				},
+			))
+			.id();
+		triangulation.insert(EntityWithPosition {
+			entity,
+			position: spade::Point2 { x, y },
+		});
+	}
+	for edge in triangulation.undirected_edges() {
+		if distribution.sample(&mut rng) {
+			let positions = edge.positions();
+			let vertices = edge.vertices();
+			let e1 = vertices[0].data().entity;
+			let e2 = vertices[1].data().entity;
+			let t1 = Vec3 {
+				x: positions[0].x,
+				y: positions[0].y,
+				z: 0.0,
+			};
+			let t2 = Vec3 {
+				x: positions[1].x,
+				y: positions[1].y,
+				z: 0.0,
+			};
+			let dist = t1.distance(t2);
+			let diff = t2 - t1;
+			let pos = (t1 + t2) / 2.0;
+			let angle = bevy::math::Quat::from_rotation_z(diff.y.atan2(diff.x)); // Rotate about the z axis
+			commands.spawn((
+				Edge,
+				Nodes {
+					entries: vec![e1, e2],
+				},
+				bevy::sprite::SpriteBundle {
+					sprite: bevy::sprite::Sprite {
+						color: bevy::render::color::Color::rgb(0.25, 0.25, 0.75),
+						custom_size: Some(bevy::math::Vec2::new(dist, 2.0)),
+						..bevy::utils::default()
+					},
+					transform: bevy::prelude::Transform {
+						translation: pos,
+						rotation: angle,
+						..bevy::utils::default()
+					},
+					..bevy::utils::default()
+				},
+			));
+		}
+	}
+}
+
+fn fix_nodes(
+	mut query: bevy::prelude::Query<(&Nodes, &mut bevy::prelude::Transform), Without<Edge>>,
+) {
 	let mut iter = query.iter_mut();
 	for i in 0..NUM_FIXED_NODES {
 		let (_, mut t) = iter.next().unwrap();
-		t.translation = Quat::from_rotation_z((i as f32) * TAU/(NUM_FIXED_NODES as f32)).mul_vec3(BARYCENTER_METHOD_RADIUS * Vec3::X);
+		t.translation = Quat::from_rotation_z((i as f32) * TAU / (NUM_FIXED_NODES as f32))
+			.mul_vec3(BARYCENTER_METHOD_RADIUS * Vec3::X);
 	}
 }
 
@@ -216,7 +304,10 @@ fn follow_nodes(
 	}
 }
 
-fn repel_nodes(mut nodes: Query<(Entity, &Nodes, &mut Movement, &Transform), Without<Configuration>>, config: Query<&Configuration>) {
+fn repel_nodes(
+	mut nodes: Query<(Entity, &Nodes, &mut Movement, &Transform), Without<Configuration>>,
+	config: Query<&Configuration>,
+) {
 	let configuration = config.single();
 	for (_, _, mut m, _) in nodes.iter_mut() {
 		m.velocity = Vec3 {
@@ -234,7 +325,9 @@ fn repel_nodes(mut nodes: Query<(Entity, &Nodes, &mut Movement, &Transform), Wit
 		m1.velocity -= repulsion;
 		m2.velocity += repulsion;
 		if (*n2).entries.contains(&e1) || (*n2).entries.contains(&e2) {
-			let force = diff * ((dist - configuration.spring_length) / configuration.spring_scale).log10() * configuration.spring_force;
+			let force = diff
+				* ((dist - configuration.spring_length) / configuration.spring_scale)
+				* configuration.spring_force;
 			m1.velocity += force;
 			m2.velocity -= force;
 		}
@@ -255,14 +348,21 @@ fn repel_nodes(mut nodes: Query<(Entity, &Nodes, &mut Movement, &Transform), Wit
 	}
 }
 
-fn tick_barycenters(mut nodes: Query<(Entity, &Nodes, &mut Transform)>, entities: &bevy::ecs::entity::Entities){
+fn tick_barycenters(
+	mut nodes: Query<(Entity, &Nodes, &mut Transform)>,
+	entities: &bevy::ecs::entity::Entities,
+) {
 	let mut rng = rand::thread_rng();
 	let index = rng.gen_range(NUM_FIXED_NODES..NUM_NODES);
 	let id = {
 		let (e, _, _) = nodes.iter_mut().nth(index).unwrap();
 		e.index()
 	};
-	let mut pos = Vec3{x: 0.0, y: 0.0, z: 0.0};
+	let mut pos = Vec3 {
+		x: 0.0,
+		y: 0.0,
+		z: 0.0,
+	};
 	let mut degree = 0.0;
 	for (_, n, t) in nodes.iter() {
 		for e in n.entries.iter() {
@@ -272,7 +372,9 @@ fn tick_barycenters(mut nodes: Query<(Entity, &Nodes, &mut Transform)>, entities
 			}
 		}
 	}
-	let (_, _, mut t) = nodes.get_mut(entities.resolve_from_id(id).unwrap()).unwrap();
+	let (_, _, mut t) = nodes
+		.get_mut(entities.resolve_from_id(id).unwrap())
+		.unwrap();
 	t.translation = pos / degree;
 }
 
@@ -305,16 +407,22 @@ fn main() {
 	bevy::prelude::App::new()
 		.add_plugins(bevy::prelude::DefaultPlugins)
 		.add_startup_system(setup)
-		.add_startup_system(apply_system_buffers.after(setup).before(make_nodes))
-		.add_startup_system(make_nodes)
-		.add_startup_system(apply_system_buffers.after(make_nodes).before(make_edges_by_proximity))
-		.add_startup_system(make_edges_by_proximity)
+		.add_startup_system(
+			apply_system_buffers
+				.after(setup)
+				.before(make_graph_delaunay),
+		)
+		.add_startup_system(make_graph_delaunay)
+		// .add_startup_system(apply_system_buffers.after(setup).before(make_nodes))
+		// .add_startup_system(make_nodes)
+		// .add_startup_system(apply_system_buffers.after(make_nodes).before(make_edges_by_proximity))
+		// .add_startup_system(make_edges_by_proximity)
 		.add_startup_system(set_config)
 		//.add_startup_system(fix_nodes.after(make_edges))
 		.add_system(repel_nodes)
 		.add_system(tick_physics)
 		.add_system(follow_nodes)
-		.add_system(get_keyboard_input)
+		// .add_system(get_keyboard_input)
 		//.add_system(tick_barycenters)
 		.run();
 }
